17ccd13b050706ff98b939dab33a85b46162ffdd
tandavala <jose.tandavala@gmail.com>
2020-11-02 15:48:53 +0100

    adding user crud tasks

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -51,3 +51,51 @@ exports.login = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getusers = async (req, res, next) => {
+  const users = await User.find({});
+  res.status(200).json({
+    data: users,
+  });
+};
+
+exports.getuser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) return next(new Error('User does not exist'));
+    res.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const update = req.body;
+    const { userId } = req.params;
+    await User.findByIdAndUpdate(userId, update);
+    const user = await User.findById(userId);
+    res.status(200).json({
+      data: user,
+      message: 'User has been updated',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    await User.findByIdAndDelete(userId);
+    res.status(200).json({
+      data: null,
+      message: 'User has been deleted',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
